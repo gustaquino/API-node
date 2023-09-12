6b01e1ecd1de3604b1ee90fefaaa995b9d7e49b3
Gustavo de Aquino Fioranti <gustavoaquinofioranti@gmail.com>
2023-09-11 23:08:38 -0300

    did base structure of database-memory

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
+
+server.post('/fotos', () => {
+  return 'something'
+})
+
+server.get('/fotos', () => {
+  return 'something'
+})
+
+server.put('/fotos/:id', () => {
+  return 'something'
+})
+
+server.delete('/fotos/:id', () => {
+  return 'something'
+})
+
 
 server.listen({
   port: 3333,
